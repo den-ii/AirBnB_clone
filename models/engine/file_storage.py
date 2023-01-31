4f9f095c5b2b083e762ee7249c820215dffd55e1
juicio8 <wisdomochiche@gmail.com>
2023-01-31 13:03:05 -0800

    Store first object fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,52 +1,34 @@
 #!/usr/bin/python3
-"""[FileStorage class]
-"""
 from os import path
 import json
-from models.base_model import BaseModel
-
 
 class FileStorage():
-    """[FileStorage]
-    """
     __file_path = "file.json"
     __objects = {}
 
-
     def all(self):
-        """[all]
-        Returns:
-            [dict]: [dictionnary containing objects]
-        """
         return FileStorage.__objects
 
     def new(self, obj):
-        """[new]
-        Args:
-            obj ([object]): [object to be created]
-        """
         _id = obj.id
         key = str(obj.__class__.__name__) + "." + _id
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """[save]
-        """
         dct = {}
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             for k, v in FileStorage.__objects.items():
                 dct[k] = v.to_dict()
-            json.dump(dct, f, indent=4)
+            json.dump(dct, f)
 
     def reload(self):
-        """[reload]
-        """
+        from models.base_model import BaseModel
         if path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r") as f:
                 obj = json.load(f)
                 dct = {}
                 for k, v in obj.items():
-                    dct[k] = self.classes[v["__class__"]](**v)
+                    dct[k] = BaseModel(**v)
                 FileStorage.__objects = dct
         else:
             return
