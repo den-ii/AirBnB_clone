5b6161403b0e5b78aa017dc7e8a3f341e06edd2a
juicio8 <wisdomochiche@gmail.com>
2023-02-01 03:27:49 -0800

    pep8 fixes

--- console.py
+++ console.py
@@ -8,7 +8,7 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """
         hbnb console class
-    	    Args:
+            Args:
                 cmd ([module]): [cmd module for command prompt]
             Returns:
                 [bool]: [true or false]
@@ -18,12 +18,15 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         """ Exits console """
         return True
+
     def do_quit(self, arg):
-        """ Exits console	"""
+        """ Exits console"""
         return True
+
     def emptyline(self):
         """ Overwriting empty line method """
         return False
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
