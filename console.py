6fac91dfc7f4985c646c3e580e3dc58cf2edf42a
juicio8 <wisdomochiche@gmail.com>
2023-02-03 01:58:05 -0800

    update console

--- console.py
+++ console.py
@@ -6,6 +6,12 @@ import cmd
 import re
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -17,7 +23,14 @@ class HBNBCommand(cmd.Cmd):
                 [bool]: [true or false]
     """
     prompt = '(hbnb) '
-    classes = {"BaseModel" : BaseModel}
+    classes = {"BaseModel": BaseModel,
+               "User": User,
+               "State": State,
+               "City": City,
+               "Amenity": Amenity,
+               "Place": Place,
+               "Review": Review}
+    
 
     def do_EOF(self, arg):
         """ Exits console """
